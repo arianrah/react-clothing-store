b0439e147e4959603f2e4dcd95e62f39605186a6
arianrah <arianrahimi@icloud.com>
2020-04-25 13:01:25 -0700

    Rough draft nav bar. fun spinny react logo looking cute. Sign up/in feature next.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css'
 
 import HomePage from './pages/homepage/HomePage'
 import ShopPage from './pages/shop/ShopPage'
+import Header from './components/Header/Header'
 
 function App() {
 	return (
 		<Router>
 			<Fragment>
+				<Header />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
