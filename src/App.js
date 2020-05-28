5a367ede97891c278ff863290ef514fb91a840fb
arianrah <arianrahimi@icloud.com>
2020-05-28 13:30:29 -0700

    Fixed homepage component rendering

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import './App.css'
 
-import HomePage from './pages/homepage/HomePage'
+import HomePage from './pages/homepage/Homepage'
 import ShopPage from './pages/shop/ShopPage'
 import Header from './components/Header/Header'
 import AccountForm from './pages/account-form/AccountForm'
